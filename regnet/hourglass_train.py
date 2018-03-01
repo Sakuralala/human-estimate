c5a454718abc4734ad09182dd9aa68d882f94197
Sakura Tony <343541994@qq.com>
2018-03-01 10:23:23 +0800

    111

--- regnet/hourglass_train.py
+++ regnet/hourglass_train.py
@@ -16,62 +16,62 @@ data = dataset.get()
 
 # Start TF
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
-sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-tf.train.start_queue_runners(sess=sess)
+with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+    tf.train.start_queue_runners(sess=sess)
 
-#Model TODO:22改为变量
-model = HourglassModel(NUM_OF_HEATMAPS)
-model.build_model(data['image'], data['gaussian_map'])
+    #Model TODO:22改为变量
+    model = HourglassModel(NUM_OF_HEATMAPS)
+    model.build_model(data['image_crop'], data['gaussian_map'])
 
-#Loss [B,320,320]  [B,320,320]
-loss = sum(model.loss)
-# Solver
-global_step = tf.Variable(0, trainable=False, name="global_step")
-#lr_scheduler = LearningRateScheduler(values=train_para['lr'], steps=train_para['lr_iter'])
-#lr = lr_scheduler.get_lr(global_step)
-lr = tf.train.exponential_decay(
-    initial_learning_rate, global_step, decay_steps=10000, decay_rate=0.5)
-#opt = tf.train.AdamOptimizer(lr, 0.9, 0.999)
-opt=tf.train.RMSPropOptimizer(lr,)
-#opt=tf.train.AdadeltaOptimizer(lr)
-train_op = opt.minimize(loss, global_step=global_step)
+    #Loss [B,320,320]  [B,320,320]
+    loss = sum(model.loss)
+    # Solver
+    global_step = tf.Variable(0, trainable=False, name="global_step")
+    #lr_scheduler = LearningRateScheduler(values=train_para['lr'], steps=train_para['lr_iter'])
+    #lr = lr_scheduler.get_lr(global_step)
+    lr = tf.train.exponential_decay(
+        INITIAL_LEARNING_RATE, global_step, decay_steps=WEIGHT_DECAY_STEPS, decay_rate=WEIGHT_DECAY_RATE)
+    #opt = tf.train.AdamOptimizer(lr, 0.9, 0.999)
+    opt=tf.train.RMSPropOptimizer(lr,epsilon=1e-8)
+    #opt=tf.train.AdadeltaOptimizer(lr)
+    train_op = opt.minimize(loss, global_step=global_step)
 
-# init weights
-sess.run(tf.global_variables_initializer())
-saver = tf.train.Saver(max_to_keep=1, keep_checkpoint_every_n_hours=4.0)
+    # init weights
+    sess.run(tf.global_variables_initializer())
+    saver = tf.train.Saver(max_to_keep=1, keep_checkpoint_every_n_hours=4.0)
 
-# snapshot dir
-if not os.path.exists(train_para['snapshot_dir']):
-    os.mkdir(train_para['snapshot_dir'])
-    print('Created snapshot dir:', train_para['snapshot_dir'])
+    # snapshot dir
+    if not os.path.exists(train_para['snapshot_dir']):
+        os.mkdir(train_para['snapshot_dir'])
+        print('Created snapshot dir:', train_para['snapshot_dir'])
 
-print(sess.run(data['gaussian_map']))
+    print(sess.run(data['gaussian_map']))
 
-ckpt = tf.train.get_checkpoint_state(train_para['snapshot_dir'])
-if ckpt and ckpt.model_checkpoint_path:
-    saver.restore(sess, ckpt.model_checkpoint_path)
-    print('reload model.')
-    # global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
-else:
-    print('No checkpoint file found')
-# Training loop
-for i in range(train_para['max_iter']):
-    _, loss_v = sess.run([train_op, loss])
+    ckpt = tf.train.get_checkpoint_state(train_para['snapshot_dir'])
+    if ckpt and ckpt.model_checkpoint_path:
+        saver.restore(sess, ckpt.model_checkpoint_path)
+        print('reload model.')
+        # global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+    else:
+        print('No checkpoint file found')
+    # Training loop
+    for i in range(train_para['max_iter']):
+        _, loss_v = sess.run([train_op, loss])
 
-    #if (i % train_para['show_loss_freq']) == 0:
-    print('Iteration %d\t Loss %.1e' % (i, loss_v))
-    if (i % train_para['show_lr_freq']) == 0:
-        print('Current lr:', sess.run(lr))
-    sys.stdout.flush()
-
-    if (i % train_para['snapshot_freq']) == 0:
-        saver.save(
-            sess, "%s/model" % train_para['snapshot_dir'], global_step=i)
-        print('Saved a snapshot.')
+        #if (i % train_para['show_loss_freq']) == 0:
+        print('Iteration %d\t Loss %.1e' % (i, loss_v))
+        if (i % train_para['show_lr_freq']) == 0:
+            print('Current lr:', sess.run(lr))
         sys.stdout.flush()
 
-print('Training finished. Saving final snapshot.')
-saver.save(
-    sess,
-    "%s/model" % train_para['snapshot_dir'],
-    global_step=train_para['max_iter'])
+        if (i % train_para['snapshot_freq']) == 0:
+            saver.save(
+                sess, "%s/model" % train_para['snapshot_dir'], global_step=i)
+            print('Saved a snapshot.')
+            sys.stdout.flush()
+
+    print('Training finished. Saving final snapshot.')
+    saver.save(
+        sess,
+        "%s/model" % train_para['snapshot_dir'],
+        global_step=train_para['max_iter'])
--- regnet/params.py
+++ regnet/params.py
@@ -7,6 +7,9 @@ BN_EPSILON = 0.001
 CONV_WEIGHT_DECAY = 0.00004
 CONV_WEIGHT_STDDEV = 0.1
 NUM_OF_HEATMAPS=22
+WEIGHT_DECAY_STEPS=30000
+WEIGHT_DECAY_RATE=0.1
+INITIAL_LEARNING_RATE=2.5e-4
 
 #train
 train_para = {
@@ -16,4 +19,3 @@ train_para = {
     'snapshot_freq': 5000,
     'snapshot_dir': 'snapshots_handsegnet'
 }
-initial_learning_rate = 0.1

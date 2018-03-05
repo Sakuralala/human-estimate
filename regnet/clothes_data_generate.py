545048274d5e012c8432ace422c3d2df4884ec92
Sakura Tony <343541994@qq.com>
2018-03-05 10:54:31 +0800

    111

--- regnet/clothes_data_generate.py
+++ regnet/clothes_data_generate.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 import csv
 from PIL import Image
-
+import sys
 
 def _int64_feature(val):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[val]))
@@ -36,16 +36,46 @@ def coor_read(coor):
 
 
 '''
+description:用以生成对应category的图片与标签，返回两个list,分别为images和labels
+args:
+    path:训练的目录
+    category:服饰的类别，为了节省内存，还是一个个来把。。。
+'''
+def data_generate_single(
+        category,
+        path='C:\\Users\\oldhen\\Downloads\\tianchi\\fashionAI_key_points_train_20180227\\train'
+):
+    csv_file = csv.reader(open(path + '\\Annotations\\train.csv', 'r'))
+
+    images=[]
+    labels=[]
+    for i, row in enumerate(csv_file):
+        #先把表头读出来
+        if i == 0:
+            header = row
+        else:
+            if category!=row[1]:
+                continue
+            if i%100==0:
+                print(i)
+                sys.stdout.flush()
+
+            img_name = path + '\\' + row[0]
+            coor = coor_read(row[2:])
+            images.append(np.asarray(Image.open(img_name),np.float32))
+            labels.append(coor)
+    return images,labels
+
+'''
 description:用以生成对应的图片与标签，返回两个dict，每个dict内部分别为5种类别的list，两个dict中的image和label一一对应
 args:
     path:训练的目录
 '''
-
-
 def data_generate(
         path='C:\\Users\\oldhen\\Downloads\\tianchi\\fashionAI_key_points_train_20180227\\train'
 ):
     csv_file = csv.reader(open(path + '\\Annotations\\train.csv', 'r'))
+    #下次不要这样写了。。。dict的hash以及多个list到后面太占内存了。。。。64gb都差点被爆了
     clothes_labels = {
         'blouse': [],
         'skirt': [],
@@ -65,25 +95,69 @@ def data_generate(
         if i == 0:
             header = row
         else:
-            img_name = path + '\\' + row[0]
             category = row[1]
-            print(category)
+            #print(category)
+            #if i%100==0:
+            #    print(i)
+            #    sys.stdout.flush()
+
+            img_name = path + '\\' + row[0]
             coor = coor_read(row[2:])
             img=Image.open(img_name)
             #在对应的类别下添加
             clothes_labels[category].append(coor)
             clothes_images[category].append(
                 np.asarray(img, np.float32))
-    return clothes_images, clothes_labels
+            return clothes_images, clothes_labels
 
 
 '''
 description:用以将images和labels转换为tensorflow的专用二进制格式便于后续读取
 args:
-    images_dict:包含
+    images:包含单个类别的images
+    labels_dict:包含单个类别的labels
+    category:类别名
+    file_path:生成的文件存放的路径
 '''
+def convert_to_tfrecords_single(
+        images,
+        labels,
+        category,
+        file_path='D:\\tfrecords\\'):
+    print("Start coverting to tfrecord.")
+    sys.stdout.flush()
+    if category==None:
+        category='tmp'
+    file_name = file_path + '\\' + category + '.tfrec'
+    num = len(images)
+    #height = images[0].shape[0]
+    #width = images[0].shape[1]
+    with tf.python_io.TFRecordWriter(file_name) as writer:
+        #2 序列化并填充至example  然后写入磁盘
+        #方向
+        #labels.shape 返回元祖  在len以取得维数
+        for i in range(num):
+            image_raw = images[i].tostring()
+            label_raw = labels[i].tostring()
+            example = tf.train.Example(
+                features=tf.train.Features(
+                    feature={
+                        #'height': _int64_feature(height),
+                        #'width': _int64_feature(width),
+                        'label_raw': _bytes_feature(label_raw),
+                        'image_raw': _bytes_feature(image_raw)
+                    }))
+            writer.write(example.SerializeToString())
+    print("Covert %s done." % category)
 
 
+'''
+description:用以将images和labels转换为tensorflow的专用二进制格式便于后续读取
+args:
+    images_dict:包含各个类别的images的字典
+    labels_dict:包含各个类别的labels的字典
+    file_path:生成的文件存放的路径
+'''
 def convert_to_tfrecords(images_dict,
                          labels_dict,
                          file_path='C:\\Users\\oldhen\\Downloads\\tianchi'):
@@ -115,5 +189,9 @@ def convert_to_tfrecords(images_dict,
     print("Covert all done.")
 
 
-result = data_generate()
-covert_to_tfrecords(result[0], result[1])
+categories=['blouse','skirt','outwear','trousers','dress']
+for cat in categories:
+    result = data_generate_single(cat)
+    convert_to_tfrecords_single(result[0], result[1],cat)
+    #for gc
+    result=[]
